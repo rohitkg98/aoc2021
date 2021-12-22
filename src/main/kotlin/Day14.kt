8ecd02f8022b580dabe9943ec05d30bcf29afc1e
Kaushal Rohit <rohit.kg98@gmail.com>
2021-12-22 19:19:31 +0530

    day 14 part 2

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -1,3 +1,34 @@
+class OptimizedPolymer(private val pairs: Map<Pair<Char, Char>, Long>) {
+    fun apply(rules: List<InsertionRule>): OptimizedPolymer {
+        val newPairs = rules.fold(mutableMapOf<Pair<Char, Char>, Long>()) { newPairs, rule ->
+            val count = pairs.getOrDefault(rule.pair, 0)
+            if (count > 0) {
+                for (pair in rule.pairs()) {
+                    newPairs[pair] = newPairs.getOrDefault(pair, 0) + count
+                }
+            }
+
+            newPairs
+        }
+
+        return OptimizedPolymer(newPairs)
+    }
+
+    fun diffOfMaxAndMin(startChar: Char): Long {
+        val counts = pairs.toList().map { it.first.second to it.second }
+            .groupBy({ it.first }, { it.second })
+            .map { it.key to it.value.sum() }.sortedBy { it.second }
+            .sortedByDescending { it.second }
+
+        val addOneIfStartChar = {count: Pair<Char, Long> ->
+            if (count.first == startChar) count.second + 1
+            else count.second
+        }
+
+        return addOneIfStartChar(counts.first()) - addOneIfStartChar(counts.last())
+    }
+}
+
 data class Polymer(val template: String) {
     private val pairs = template.zipWithNext()
 
@@ -6,6 +37,8 @@ data class Polymer(val template: String) {
 
         return Polymer(newPairs.toTemplate())
     }
+
+    fun optimized() = OptimizedPolymer(pairs.groupingBy { it }.eachCount().mapValues { it.value.toLong() })
 }
 
 private fun List<Pair<Char, Char>>.toTemplate(): String {
@@ -23,34 +56,32 @@ object Day14 : Day() {
         val parsedRules = rules.split("\n").map {
             val (pairs, mid) = it.split(" -> ")
             InsertionRule(pairs[0] to pairs[1], mid[0])
-        }.associateBy { it.pair }
+        }
 
         val polymer = Polymer(template)
 
-        part1(polymer, parsedRules)
+        part1(polymer, parsedRules.associateBy { it.pair })
 
         part2(polymer, parsedRules)
     }
 
-    private fun part2(polymer: Polymer, parsedRules: Map<Pair<Char, Char>, InsertionRule>) {
-        polymerizeNTimes(polymer, parsedRules, 40)
+    private fun part2(polymer: Polymer, rules: List<InsertionRule>) {
+        val optimized = polymer.optimized()
+
+        val final = (1..40).fold(optimized) { p, _ -> p.apply(rules) }
+
+        val diff = final.diffOfMaxAndMin(polymer.template[0])
+
+        println("the difference b/w max occurring and least occurring after polymerizing 40 times: $diff")
     }
 
     private fun part1(polymer: Polymer, rules: Map<Pair<Char, Char>, InsertionRule>) {
-        polymerizeNTimes(polymer, rules, 10)
-    }
-
-    private fun polymerizeNTimes(
-        polymer: Polymer,
-        rules: Map<Pair<Char, Char>, InsertionRule>,
-        n: Int
-    ) {
-        val final = (1..n).fold(polymer) { p, _ -> p.apply(rules) }
+        val final = (1..10).fold(polymer) { p, _ -> p.apply(rules) }
 
         val count = final.template.groupingBy { it }.eachCount()
         val max = count.maxOf { it.value }
         val min = count.minOf { it.value }
 
-        println("the difference b/w max occuring and least occuring after polymerizing $n times: ${max - min}")
+        println("the difference b/w max occurring and least occuring after polymerizing 10 times: ${max - min}")
     }
 }
